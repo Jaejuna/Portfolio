dd5b7426ad3d5d1e2ed07a874ef8b55f6b138c5a
jaejuna <anayana9988@gmail.com>
2020-09-12 18:07:39 +0900

    JS home contact btn

--- main.js
+++ main.js
@@ -12,3 +12,29 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar_dark');
     }
 });
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+    scrollIntoView(link);
+});
+
+
+//Handle scrolling when tapping on the "contact me"
+const homeContactMe = document.querySelector('.home_contact');
+homeContactMe.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
+//ScrollIntoView fucntion
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth", inline: "start"});
+}
